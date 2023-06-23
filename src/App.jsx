a5f1ea8941521018b51497a4dbb569833ad7c5c7
Felipe Poletti <felipepolettig@gmail.com>
2023-06-22 22:42:57 -0300

    Learning about useRef hook and insert letter on the game

--- src/App.jsx
+++ src/App.jsx
@@ -52,8 +52,8 @@ function App() {
     setGameStage(stages[1].name);
   };
 
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    console.log(letter);
   };
 
   const retry = () => {
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from "react";
 import "./Game.css";
 
 const Game = ({ 
@@ -10,6 +11,18 @@ const Game = ({
     guesses, 
     score 
 }) => {
+    const [letter, setLetter] = useState("");
+    const letterInputRef = useRef(null);
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      verifyLetter(letter);
+
+      setLetter("");
+
+      letterInputRef.current.focus();
+    };
+
     return (
         <div className="game">
             <p className="points">
@@ -29,8 +42,16 @@ const Game = ({
             </div>
             <div className="letterContainer">
                 <p>Tente adivinhar uma letra:</p>
-                <form>
-                    <input type="text" name="letter" maxLength="1" required/>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                      type="text" 
+                      name="letter" 
+                      maxLength="1" 
+                      required 
+                      onChange={(e) => setLetter(e.target.value)}
+                      value={letter}
+                      ref={letterInputRef}
+                    />
                     <button>Jogar</button>
                 </form>
             </div>
