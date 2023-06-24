0ec9ec27b016d3007ee4b9635590921e463ae73e
Felipe Poletti <felipepolettig@gmail.com>
2023-06-24 19:48:25 -0300

    finishing end game

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === 'end' &&  <GameOver retry={retry}/>}
+      {gameStage === 'end' &&  <GameOver retry={retry} score={score} />}
     </div>
   )
 }
